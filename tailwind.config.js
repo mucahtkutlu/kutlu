ec1e35c75146de62890570dba6c5b425ca248a56
Mucahit Kutlu <mucahtkutlu@gmail.com>
2023-11-28 00:47:37 +0100

    fuckOff3

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,15 +10,22 @@ module.exports = {
     },
     extend: {
       colors: {
-        darkBlue: "#06113C",
-        Orange: "#FF8C32",
-        darkGrey: "#DDDDDD",
-        brownishGreen: "#595519",
-        Grey: "#EEEEEE"
+        warmRed: "#c7522a",
+        warmBrown: "#e5c185",
+        warmGreen: "#74a892",
+        warmOrange: "#FFA500",
+        warmBlue: "#008585",
+        warmWhite: "#fbf2c4",
+        lighterGrey: "#C4C4C4",
+        mostlighterGrey: "#E3E3E3"
       },
 
       backgroundImage: {
-        "hero": "url('/img/hero.jpg')",
+        "hero": "url('/img/hero 3.jpg')",
+      },
+
+      spacing: {
+        '900': '64rem',
       }
     },
   },
